2ed306d1c8167c50671b4623f6183a70dda2e2eb
E132 <107452563+132E@users.noreply.github.com>
2022-09-10 16:53:05 -0500

    home button adjustments

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,8 +6,10 @@ const Home  = () => {
             <div className="cta">
                 <h2>Qwixs</h2>
                 <h1>Simplify your online presence!</h1>
-                <NavLink className="cta-links" to="/signup">Sign Up</NavLink>
-                <NavLink className="cta-links" to="/login">Login</NavLink>
+                <div className="cta-buttons">
+                    <NavLink className="cta-links" to="/signup">Sign Up</NavLink>
+                    <NavLink className="cta-links" to="/login">Login</NavLink>
+                </div>
             </div>
         </section>
     )
