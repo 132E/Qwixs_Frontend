91221eb14d82dd6e3c255ff804c6db1f926046b6
E132 <107452563+132E@users.noreply.github.com>
2022-09-13 18:18:59 -0500

    add service form ready

--- client/src/components/AddServices.jsx
+++ client/src/components/AddServices.jsx
@@ -1,7 +1,64 @@
-const AddServices = () => {
+import {useState} from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import axios from 'axios';
+import { BASE_URL } from '../globals';
+
+
+const AddServices = ({getAllBusinesses}) => {
+
+    let navigate = useNavigate();
+    let location = useLocation();
+    let businessId = location.state.businessId;
+
+    const initialStateService = {
+        name: '',
+        description: '',
+    }
+
+    const [serviceFormState, setServiceFormState] = useState(initialStateService);
+
+    const handleChangeService = (event) => {
+        setServiceFormState({
+            ...serviceFormState,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubmitService = async (event) => {
+        event.preventDefault();
+        let res = await axios.post(
+            `${BASE_URL}/services/${businessId}`,
+            serviceFormState
+        )
+        setServiceFormState(initialStateService);
+        getAllBusinesses();
+        navigate(`/businesses/${businessId}`); // go back to business page
+    }
+
+
     return (
-        <div>
+        <div className='main-container'>
             <h1>Add Services</h1>
+            <div className='form-container'>
+                <form onSubmit={handleSubmitService}>
+                    <input
+                        id="name"
+                        type="text"
+                        onChange={handleChangeService}
+                        value={serviceFormState.name}
+                        placeholder="Name"
+                    />
+                    <input
+                        id="description"
+                        type="text"
+                        onChange={handleChangeService}
+                        value={serviceFormState.description}
+                        placeholder="Description"
+                    />
+                    <button type="submit">Add Service</button>
+
+                </form>
+            </div>
         </div>
     );
 };
