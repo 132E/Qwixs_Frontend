7120fd9d081b6943da0efd34300be807698c1f6d
E132 <107452563+132E@users.noreply.github.com>
2022-09-13 09:39:13 -0500

    added services to business list

--- client/src/pages/BusinessList.jsx
+++ client/src/pages/BusinessList.jsx
@@ -1,4 +1,4 @@
-const BusinessList = ({ businesses,  }) => {
+const BusinessList = ({ businesses, chooseBusiness }) => {
     return (
         <section className="page" id="business-list">
                 <h1>Business List</h1>
@@ -10,7 +10,10 @@ const BusinessList = ({ businesses,  }) => {
                             key={business._id}>
                                 <h3>{business.name}</h3>
                                 <p>{business.about}</p>
-                                <h5>{business.Services[0].name}</h5>
+                            
+                                { business.Services?.map((service) => (
+                                    <h5 key={service.name}>{service.name}</h5>
+                                ))}
                                 <p>{business.phone}</p>
                                 <p>{business.address}</p>
                                 <p>{business.city}</p>
